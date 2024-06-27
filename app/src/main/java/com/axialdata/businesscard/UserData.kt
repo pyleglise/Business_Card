2e7b4683020380a5d112a853d99ccd561ed26a36
Pierre-Yves Léglise <contact@axialdata.net>
2024-06-27 18:14:33 -0400

    Debut de dev

---
+++ app/src/main/java/com/axialdata/businesscard/UserData.kt
@@ -0,0 +1,10 @@
+package com.axialdata.businesscard
+
+object UserData {
+    const val firstName: String = "Pierre-Yves"
+    const val lastName: String = "Léglise"
+    const val title:String = "Consultant SI"
+    const val email: String = "contact@axialdata.net"
+    const val phone: String = "+596 696 36 77 41"
+    const val social: String = "@pierre-yves-leglise"
+}
